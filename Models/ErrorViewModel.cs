77bfad225e7d6c9743ed222bb4768b83e5f6fa57
mrouffelaers <mrouffelaer@ensc.fr>
2021-11-19 13:12:46 +0100

    Création MvcUniversity et .gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
