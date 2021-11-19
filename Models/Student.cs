964f776019783636f15a8ce767b4a00b815cb8bc
mrouffelaers <mrouffelaer@ensc.fr>
2021-11-19 15:26:02 +0100

    modif student

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,13 +7,15 @@ namespace MvcUniversity.Models
     public class Student
     {
         public int Id { get; set; }
+        
         [Display(Name = "Last Name"), Required]
         public string LastName { get; set; }
 
-        [Display(Name ="Enrollment Date"), DataType(DataType.Date)]
-        public DateTime EnrollmentDate { get; set; }
         [Display(Name = "First Name"), Required]
         public string FirstName { get; set; }
+
+        [Display(Name ="Enrollment Date"), DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
